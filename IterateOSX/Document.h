fe30f8c2ccdce463ae017f8010af7dd5530abfd6
James Wilson <jwils22@gmail.com>
2014-09-23 05:17:53 -0400

    Added document support

---
+++ IterateOSX/Document.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  IterateOSX
+//
+//  Created by James Wilson on 9/23/14.
+//  Copyright (c) 2014 Noesis Ingenuity LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@property (copy) NSMutableArray *layers;
+
+@end
